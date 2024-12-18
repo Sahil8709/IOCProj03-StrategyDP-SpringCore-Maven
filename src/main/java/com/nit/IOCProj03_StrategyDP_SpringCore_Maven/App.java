523e9b5f18a7eb59ca193a21552c869509b5d153
Sahil8709 <sahilraza87097@gmail.com>
2024-12-19 00:24:32 +0530

    first commit

---
+++ src/main/java/com/nit/IOCProj03_StrategyDP_SpringCore_Maven/App.java
@@ -0,0 +1,10 @@
+package com.nit.IOCProj03_StrategyDP_SpringCore_Maven;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
